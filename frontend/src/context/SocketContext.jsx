6e1aafbf5d93a8c8d570911fae68ea64f8b7f44b
Kavya Sai Isheka <yakkalakavyasaiisheka@gmail.com>
2024-02-18 19:11:21 +0530

    completed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-m6ae.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
